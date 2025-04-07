540928a96a41bfc26f4044a1c98570341ab7f8e8
Piotr Wieleba <wieleba.piotr@gmail.com>
2025-04-07 14:38:47 +0200

    initial commit

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,6 +1,8 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import {fileURLToPath} from 'node:url';
 import * as Repack from '@callstack/repack';
+import fs from 'fs';
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,17 +14,51 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-export default {
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(),
-  },
-  module: {
-    rules: [
-      ...Repack.getJsTransformRules(),
-      ...Repack.getAssetTransformRules(),
+export default env => {
+  const {platform, mode} = env;
+
+  return {
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    // output: {
+    //   uniqueName: 'repack-host-app',
+    // },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules(),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin({
+        platform,
+      }),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'HostApp',
+        filename: 'RepackHostApp.container.js.bundle',
+        dts: false,
+        remotes: {
+          ChildApp: `ChildApp@http://localhost:9000/${platform}/ChildApp.container.js.bundle`,
+        },
+        shared: Object.fromEntries(
+          Object.entries(pkg.dependencies).map(([dep, {version}]) => {
+            return [
+              dep,
+              {singleton: true, eager: true, requiredVersion: version},
+            ];
+          }),
+        ),
+      }),
+      // Supports for new architecture - Hermes can also use JS, it's not a requirement,
+      // it will still work the same but it's for performance optimization
+      new Repack.plugins.HermesBytecodePlugin({
+        enabled: mode === 'production',
+        test: /\.(js)?bundle$/,
+        exclude: /index.bundle$/,
+      }),
     ],
-  },
-  plugins: [new Repack.RepackPlugin()],
+  };
 };
